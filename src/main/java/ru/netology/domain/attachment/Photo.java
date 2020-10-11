14b91ea15f38c327928bc4500e0d78fd2d8dcf39
Veronazavr <verochka10@list.ru>
2020-10-11 15:37:55 +0300

    added updated Photo

---
+++ src/main/java/ru/netology/domain/attachment/Photo.java
@@ -0,0 +1,11 @@
+package ru.netology.domain.attachment;
+
+public class Photo {
+    String text;
+    int photoID;
+    int ownerID;
+    int userID;
+    int date;
+    int width;
+    int height;
+}
