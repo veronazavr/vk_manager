d2532d01f58460b1fc9a52d0a42efdedfaa4bc43
Veronazavr <verochka10@list.ru>
2020-10-11 13:38:10 +0300

    added updated post

--- src/main/java/ru/netology/domain/post.java
+++ src/main/java/ru/netology/domain/post.java
@@ -1,7 +1,40 @@
 package ru.netology.domain;
 
 public class post {
+
     private int id;
+    private int ownerID;
+    private int fromID;
+    private int createdBy;
+    private int date;
+    private String mainText;
+    private int replyOwnerID;
+    private int replyPostID;
+    private boolean friendsOnly;
+    private String commentButton;
+    private int commentCount;
+    private boolean commentCanPost;
+    private boolean commentCanClose;
+    private boolean commentCanOpen;
+    private String copyright;
+    private int likesCount;
+    private boolean userLikes;
+    private boolean canLike;
+    private boolean canPublish;
+    private int repostsCount;
+    private boolean userReposted;
+    private int viewsCount;
+    private String postType;
+    private String postSource;
+    private String geo;
+    private int signerID;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean pinned;
+    private boolean markedAsAd;
+    private boolean favorite;
+    private int postponedID;
 
     public int getId() {
         return id;
@@ -266,37 +299,6 @@ public class post {
     public void setPostponedID(int postponedID) {
         this.postponedID = postponedID;
     }
-
-    private int ownerID;
-    private int fromID;
-    private int createdBy;
-    private int date;
-    private String mainText;
-    private int replyOwnerID;
-    private int replyPostID;
-    private boolean friendsOnly;
-    private String commentButton;
-    private int commentCount;
-    private boolean commentCanPost;
-    private boolean commentCanClose;
-    private boolean commentCanOpen;
-    private String copyright;
-    private int likesCount;
-    private boolean userLikes;
-    private boolean canLike;
-    private boolean canPublish;
-    private int repostsCount;
-    private boolean userReposted;
-    private int viewsCount;
-    private String postType;
-    private String postSource;
-    private String geo;
-    private int signerID;
-    private boolean canPin;
-    private boolean canDelete;
-    private boolean canEdit;
-    private boolean pinned;
-    private boolean markedAsAd;
-    private boolean favorite;
-    private int postponedID;
 }
+
+
