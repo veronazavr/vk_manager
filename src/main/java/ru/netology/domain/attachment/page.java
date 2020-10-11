5a3fa996c7997e99acf9d508c580dd2cb6e62111
Veronazavr <verochka10@list.ru>
2020-10-11 14:15:39 +0300

    added updated page

---
+++ src/main/java/ru/netology/domain/attachment/page.java
@@ -0,0 +1,20 @@
+package ru.netology.domain.attachment;
+
+public class page {
+
+    int id;
+    int groupID;
+    int creatorID;
+    String title;
+    boolean canEdit;
+    boolean canAccess;
+    int canView;
+    int edited;
+    int created;
+    int editorID;
+    int views;
+    String parent;
+    String source;
+    String html;
+    String viewURL;
+}
---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+
+    }
+
